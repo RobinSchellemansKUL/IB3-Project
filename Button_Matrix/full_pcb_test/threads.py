b6efd088ef887789f00ea2e8a9cc66e8c5b85d82
Matan <matan.putzeys@student.kuleuven.be>
2025-04-02 11:18:47 +0200

    update: fixed button matrix

--- Button_Matrix/full_pcb_test/threads.py
+++ Button_Matrix/full_pcb_test/threads.py
@@ -3,12 +3,10 @@ import time
 import pygame
 import threading
 
-from input_output import polling_bpm, polling_matrix
+from input_output import polling_bpm
 from sequencer import play_sequencer
 
 import main
-
-event = threading.Event()
 
 def thread_volume():
     global thread_volume
@@ -20,12 +18,6 @@ def thread_bpm():
     if main.thread_bpm is None or not main.thread_bpm.is_alive():
         main.thread_bpm = threading.Thread(target=polling_bpm, daemon=True)
         main.thread_bpm.start()
-
-def thread_button_matrix():
-    global event
-    if main.thread_button_matrix is None or not main.thread_button_matrix.is_alive():
-        main.thread_button_matrix = threading.Thread(target=polling_matrix, args=(event,), daemon=True)
-        main.thread_button_matrix.start()
 
 def thread_sequencer():
     if main.thread_sequencer is None or not main.thread_sequencer.is_alive():
